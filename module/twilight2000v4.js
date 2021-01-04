fe2de5e9c67914becb6fe8acdb6acfa7b9a2279e
cody <redpisces@live.com>
2021-01-04 17:17:16 -0500

    initial

---
+++ module/twilight2000v4.js
@@ -0,0 +1,47 @@
+// Import Modules
+import { twilightActor } from "./actor/actor.js";
+import { twilightActorSheet } from "./actor/actor-sheet.js";
+import { twilightItem } from "./item/item.js";
+import { twilightItemSheet } from "./item/item-sheet.js";
+
+Hooks.once('init', async function() {
+
+  game.twilight2000v4 = {
+    twilightActor,
+    twilightItem
+  };
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d20",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = twilightActor;
+  CONFIG.Item.entityClass = twilightItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("twilight2000v4", twilightActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("twilight2000v4", twilightItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+});
