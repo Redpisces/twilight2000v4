75b439e02807defdb7930a1fa151fcd7bfb07a74
redpisces <redpisces@live.com>
2021-01-22 13:32:04 -0500

    test slots

---
+++ module/templates.js
@@ -0,0 +1,46 @@
+
+/**
+ * Defines a set of template paths to pre-load.
+ * Pre-loaded templates are compiled and cached for fast access when rendering.
+ * @return {Promise}
+ */
+export const preloadHandlebarsTemplates = async function() {
+	return loadTemplates([
+		// Shared Partials
+		// 'templates/dice/roll.html',
+
+		// Actor Sheet Partials
+		'systems/twilight2000v4/templates/actor/part/attribute-slot.hbs'
+	]);
+}
+
+export function registerHandlebars(){
+	// If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+  
+  Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+        
+    return {
+        "+": lvalue + rvalue,
+        "-": lvalue - rvalue,
+        "*": lvalue * rvalue,
+        "/": lvalue / rvalue,
+        "%": lvalue % rvalue
+    }[operator];
+  });	
+}
+		
--- module/twilight2000v4.js
+++ module/twilight2000v4.js
@@ -3,7 +3,7 @@ import { twilightActor } from "./actor/actor.js";
 import { twilightActorSheet } from "./actor/actor-sheet.js";
 import { twilightItem } from "./item/item.js";
 import { twilightItemSheet } from "./item/item-sheet.js";
-
+import { preloadHandlebarsTemplates, registerHandlebars } from './module/templates.js';
 Hooks.once('init', async function() {
 
   game.twilight2000v4 = {
@@ -30,31 +30,5 @@ Hooks.once('init', async function() {
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("twilight2000v4", twilightItemSheet, { makeDefault: true });
 
-  // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
-
-  Handlebars.registerHelper('toLowerCase', function(str) {
-    return str.toLowerCase();
-  });
   
-  Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
-    lvalue = parseFloat(lvalue);
-    rvalue = parseFloat(rvalue);
-        
-    return {
-        "+": lvalue + rvalue,
-        "-": lvalue - rvalue,
-        "*": lvalue * rvalue,
-        "/": lvalue / rvalue,
-        "%": lvalue % rvalue
-    }[operator];
-  });
 });
