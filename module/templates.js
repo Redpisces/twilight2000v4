8b6ff24816fc68c7e44876f9d483a6df4177f6e0
redpisces <redpisces@live.com>
2021-01-22 13:45:46 -0500

    test slots

--- module/templates.js
+++ module/templates.js
@@ -11,6 +11,7 @@ export const preloadHandlebarsTemplates = async function() {
 
 		// Actor Sheet Partials
 		'systems/twilight2000v4/templates/actor/part/attribute-slot.hbs'
+		'systems/twilight2000v4/templates/actor/part/skill-slot.hbs'
 	]);
 }
 
